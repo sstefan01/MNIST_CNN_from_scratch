81e8e06e0d3d0f776d21ea0b5c74d93488918757
sstefan01 <sabina_stefan@brown.edu>
2020-02-11 21:59:07 -0500

    Vectorized/ found bugs
    Found bugs in backprop
    Replaced for loops

---
+++ dconv_back.m
@@ -0,0 +1,32 @@
+%for padding = 1; stride = 1
+function res = dconv_back(x,kern)
+
+    rc = floor(((size(x(:,:,1,1)) - size(kern(:,:,1,1)))) + 1);
+    ind = reshape(1:size(x,1)*size(x,2),size(x,1),size(x,2));
+    C = im2col(ind, size(kern(:,:,1,1)), 'sliding');
+
+
+    n = numel(x(:,:,1,1));
+    l = 0:size(x,3)-1;
+    l = l*n;
+    l = reshape(l,1,1,length(l));
+
+    C = repmat(C, 1,1, size(x,3));
+    C = C + l;
+
+    C = repmat(C,1,1,1,size(x,4));
+    l2 = 0:size(x,4)-1;
+    l2 = l2*n*size(x,3);
+    %l2 = repmat(C(end,end,end,1),1,1,1,size(x,4));
+    l2 = reshape(l2,1,1,1,length(l2));
+    C = C+l2;
+
+
+    k = reshape(kern,numel(kern(:,:,1,1)),1,size(kern,3),size(kern,4));
+    k = repmat(k,1,1,1,1,size(x,4));
+    xC = x(C);
+    xC = reshape(xC,size(xC,1),size(xC,2),1,size(xC,3),size(xC,4));
+    res = reshape(sum(sum(xC.*k,1),4), rc(1), rc(2),size(kern,3), size(x,4));
+
+end
+
